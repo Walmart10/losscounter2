2f13c92b2aa2f6a6f3c1b58a02850b688713a13f
Walmart10 <maxwellhay1@gmail.com>
2020-09-24 21:01:35 -0600

    Some random comment

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
-import { Pool } from "pg";
+import { Client } from "pg";
 
-const pool = new Pool("postgres://eymymdky:z5RIT9jYL-nTlgUIxcer-_w4iOZZk3Rq@lallah.db.elephantsql.com:5432/eymymdky")
+const client = new Client("postgres://eymymdky:z5RIT9jYL-nTlgUIxcer-_w4iOZZk3Rq@lallah.db.elephantsql.com:5432/eymymdky")
 
-pool
+client.connect();
+
+client.query("select * from people", (err, res) => {
+  if (err) {
+    throw err;
+  } else {
+    for (let row of res.rows) {
+      console.log(row)
+    }
+    client.end();
+  }
+});
