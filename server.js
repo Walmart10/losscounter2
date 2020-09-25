b34d887e12bd35f23b48891bcd5800a411f40a85
Walmart10 <maxwellhay1@gmail.com>
2020-09-24 21:42:33 -0600

    Got SQL queries working

--- server.js
+++ server.js
@@ -1,16 +1,29 @@
-import { Client } from "pg";
+import { Pool } from "pg";
 
-const client = new Client("postgres://eymymdky:z5RIT9jYL-nTlgUIxcer-_w4iOZZk3Rq@lallah.db.elephantsql.com:5432/eymymdky")
-
-client.connect();
-
-client.query("select * from people", (err, res) => {
-  if (err) {
-    throw err;
-  } else {
-    for (let row of res.rows) {
-      console.log(row)
-    }
-    client.end();
-  }
+const pool = new Pool({
+  user: "eymymdky",
+  host: "lallah.db.elephantsql.com",
+  database: "eymymdky",
+  password: "z5RIT9jYL-nTlgUIxcer-_w4iOZZk3Rq",
+  port: 5432
 });
+
+pool.on("error", (err, client) => {
+  console.error(err);
+  process.exit(-1);
+});
+
+pool.connect((err, client, done) => {
+  if (err) throw err;
+  client.query("SELECT * FROM people", (err, res) => {
+    done();
+
+    if (err) {
+      console.log(err.stack);
+    } else {
+      for (let row of res.rows) {
+        console.log(row);
+      }
+    }
+  })
+})
